6d3105cfb3146e158187d90cfa883c56176ea511
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2025-09-17 16:56:58 +0500

    add gist route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { GistComponent } from './public-gists/gist/gist.component';
 import { PublicGistsComponent } from './public-gists/public-gists.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: PublicGistsComponent
+  },
+  {
+    path: 'gist/:id',
+    component: GistComponent
   }
 ];
