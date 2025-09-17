0581fc2f142ebd5b78c349751a1d1eb5463453d7
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2025-09-17 16:58:43 +0500

    add list view with pagination

--- src/app/public-gists/list-view/list-view.component.ts
+++ src/app/public-gists/list-view/list-view.component.ts
@@ -1,36 +1,63 @@
-import { Component, OnDestroy } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { Gist, GistService } from '../../gist.service';
 import { CommonModule } from '@angular/common';
-import { TimeAgoPipe } from '../../time-ago.pipe';
+import { Component, OnDestroy } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Subject, takeUntil } from 'rxjs';
+import { Gist, GistService } from '../../gist.service';
+import { TimeAgoPipe } from '../../time-ago.pipe';
 
 @Component({
   selector: 'app-list-view',
-  imports: [MatTableModule, CommonModule, TimeAgoPipe],
+  imports: [MatTableModule, MatProgressSpinnerModule, CommonModule, TimeAgoPipe],
   templateUrl: './list-view.component.html',
   styleUrl: './list-view.component.scss'
 })
 export class ListViewComponent implements OnDestroy {
   displayedColumns: string[] = ['owner', 'description', 'files', 'updated'];
   dataSource = new MatTableDataSource<Gist>([]);
-  
+  isLoading = false;
+  currentPage = 1;
+
   private destroy$ = new Subject<void>();
 
   constructor(private gistService: GistService) {}
 
   ngOnInit() {
-    // Subscribe to gists from the service
-    this.gistService.gists$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(gists => {
-        this.dataSource.data = gists;
-      });
+    this.gistService.gists$.pipe(takeUntil(this.destroy$)).subscribe((gists) => {
+      this.dataSource.data = gists;
+    });
+
+    this.gistService.page$.pipe(takeUntil(this.destroy$)).subscribe((page) => {
+      this.currentPage = page;
+    });
+
+    this.gistService.loading$.pipe(takeUntil(this.destroy$)).subscribe((isLoading) => {
+      this.isLoading = isLoading;
+    });
   }
 
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  nextPage(): void {
+    this.gistService.setPage(this.currentPage + 1);
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.gistService.setPage(this.currentPage - 1);
+    }
+  }
+
+  get hasNextPage(): boolean {
+    // Assume there's always a next page unless we get less than 8 results
+    return this.dataSource.data.length === 8;
+  }
+
+  get hasPrevPage(): boolean {
+    return this.currentPage > 1;
   }
 
   getFirstFileName(files: { [key: string]: any }): string {
