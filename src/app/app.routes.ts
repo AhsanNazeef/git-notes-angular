8bcf3774ca2fba470ce9dd3eff1a038ed91f21ef
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2025-09-18 15:32:46 +0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { CreateGistComponent } from './create-gist/create-gist.component';
+import { ProfileComponent } from './profile/profile.component';
 import { GistComponent } from './public-gists/gist/gist.component';
 import { PublicGistsComponent } from './public-gists/public-gists.component';
 
@@ -10,5 +12,13 @@ export const routes: Routes = [
   {
     path: 'gist/:id',
     component: GistComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'create',
+    component: CreateGistComponent
   }
 ];
